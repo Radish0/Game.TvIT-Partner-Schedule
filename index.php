fcc5ebf0ee71eaabe226139115cf0515d4992aac
Radish0 <78257549+Radish0@users.noreply.github.com>
2021-05-06 13:49:44 +0200

    Upload php

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE HTML>
+<?php 
+require("Https.php"); 
+?>
+<html>
+
+	<head>
+		<title>Game.Tv It League</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Work with us</a></li>
+							<li><a href="#two">About us</a></li>
+							<li><a href="#three">Weekly Ranking</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Game.Tv It League</h1>
+							<p>The 1# Mobile Esports Platform hosting thousand tournaments everyday<br>
+							Completely free, Prizepool for everyone, simple and intuitive</p>
+							<ul class="actions">
+								<li><a href="https://www.game.tv/it" class="button scrolly">Learn more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Do you already work with us?</h2>
+									<p>Click the button below and Log into the host dashboard. You can also regist to an existing community</p>
+									<ul class="actions">
+										<li><a href="Login.html" class="button">Enter in the dashboard</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Do you have a community?</h2>
+									<p>Do you have a community? Do you want to organize prize tournaments? <br/>
+										Send a partner request and our managers will analyze it!</p>
+									<ul class="actions">
+										<li><a href="Register.html" class="button">Submit a request</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+                        <section>
+							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Are you in an already registered community?</h2>
+									<p>Are you in an already registered community? Do you want to request access to your community? <br/>
+									<ul class="actions">
+										<li><a href="Request.html" class="button">Submit a request</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>About us</h2>
+							<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet imperdiet est velit quis lorem.</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>3M+ Downloads</h3>
+									<p>The platform of choice for the gaming industry and community at large.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-gem"></span>
+									<h3>17+ Geographies</h3>
+									<p>Interact with highly engaged gamers from over 17 Geographies.Target also by demographics, language, game preference and so much more.</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-user"></span>
+									<h3>Over 200k daily players</h3>
+									<p>Interact with highly engaged gamers.</p>
+								</section>
+								<section>
+									<span class="icon major fa-flag"></span>
+									<h3>2000+ events daily</h3>
+									<p>Auto-tournaments create easier play for larger crowds, making users eager to return.</p>
+								</section>
+							</div>
+					
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Weekly ranking</h2>
+							<p>Weekly brawl stars Ranking based on the Italian Championship</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Name</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Email</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Message</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Send Message</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<span>12345 Somewhere Road #654<br />
+											Nashville, TN 00000-0000<br />
+											USA</span>
+										</li>
+										<li>
+											<h3>Email</h3>
+											<a href="#">user@untitled.tld</a>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<span>(000) 000-0000</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+												<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+												<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; All rights reserved.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
