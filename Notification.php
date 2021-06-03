2af1baf07f00b604a731b29b6ff1e5ad563747be
Radish0 <78257549+Radish0@users.noreply.github.com>
2021-06-03 13:12:38 +0200

    Update Notification.php

--- Notification.php
+++ Notification.php
@@ -11,6 +11,10 @@ if (!isset($_REQUEST["Mail"]) || strlen($_REQUEST["Mail"])==0) errore(400,"Param
 
 if (!isset($_REQUEST["Stato"]) || strlen($_REQUEST["Stato"])==0) errore(400,"Parametri errati. state expected");
 
+if (!isset($_REQUEST["Name"]) || strlen($_REQUEST["Name"])==0) errore(400,"Parametri errati. Nome expected");
+
+echo("ciao");
+
 try {
 	$hostname = "localhost";
     $dbname = "my_radicig";
@@ -20,8 +24,7 @@ try {
 } catch (Exception $e) {
   errore(500,"SERVER ERROR. PDO connection failed: " . $e->getMessage());
 }
-echo $_REQUEST["State"];
-echo $_REQUEST["Mail"];
+
 $sql = $db->prepare("UPDATE community SET Stato = ? WHERE Mail = ?");
 try
 { 
@@ -29,7 +32,60 @@ try
   
   http_response_code(200);
   header("Content-type: application/json; charset: UTF-8");
-  exit;}
+  
+if($_REQUEST["Stato"]=="Approved"){
+  $to = $_REQUEST["Mail"]; // note the comma
+
+  // Subject
+  $subject = 'Game.tv Partner';
+
+  // Message
+  $message = '
+  <html>
+  <head>
+    <title>Game.tv accepts your request for a new Partner</title>
+  </head>
+  <body>
+    <p>Congratulation! Click the link below to start.</p>
+    <a href=https://radicig.altervista.org/Esame/ReqToAcommunity.php?Nome='.$_REQUEST["Name"].'&Mail='.$_REQUEST["Mail"].'>Click here</a>
+  </body>
+  </html>
+  ';
+
+  // To send HTML mail, the Content-type header must be set
+	$headers = 'From: Game.tv <partners@game.tv>' . "\r\n" .'Reply-To: noreplytothis@game.tv' . "\r\n" .'X-Mailer: PHP/' . phpversion() . "\r\n" .'Content-type: text/html';
+
+
+  // Mail it
+  mail($to, $subject, $message,$headers);
+} else{
+$to = $_REQUEST["Mail"]; // note the comma
+
+  // Subject
+  $subject = 'Game.tv Partner';
+
+  // Message
+  $message = "
+  <html>
+  <head>
+    <title>Game.tv refuse your request for a new Partner</title>
+  </head>
+  <body>
+  	<p>Your partner have been refused</p>
+    <p>We are sorry but your community doesn't reflect our standard</p>
+  </body>
+  </html>
+  ";
+
+  // To send HTML mail, the Content-type header must be set
+  $headers = 'From: Game.tv <partners@game.tv>' . "\r\n" .'Reply-To: noreplytothis@game.tv' . "\r\n" .'X-Mailer: PHP/' . phpversion() . "\r\n" .'Content-type: text/html';
+
+
+  // Mail it
+  mail($to, $subject, $message, $headers);
+
+}
+  }
 catch(PDOException $e)
 {
   errore(404,"database not exists");
